dd5bc0112f699ef850190a1b992d92c24235198a
Nandu BS <96563833+Nandubs@users.noreply.github.com>
2025-02-07 12:04:13 +0530

    Init commit

---
+++ src/data/educationData.js
@@ -0,0 +1,24 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: 'APJ Abdul Kalam Technological University, Kerala',
+        course: 'Computer Science and Engineering',
+        startYear: '2016',
+        endYear: '2020'
+    },
+    {
+        id: 1,
+        institution: 'Sree Narayana Vilasam Higher Secondary School, Varkala',
+        course: 'Higher Secondary - Computer Science',
+        startYear: '2014',
+        endYear: '2016'
+    },
+    {
+        id: 3,
+        institution: 'Sree Sethu Parvathi Bai Higher Secondary School, Kadakkavur',
+        course: 'High School - Kerala State Syllabus',
+        startYear: '2011',
+        endYear: '2014'
+    },
+
+]
